2440d38a5971fa55b1efc598d14e2881e134444a
Donnie Mattingly <donniewmattingly@gmail.com>
2020-12-17 07:43:32 -0500

    add grommet for styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {StoreProvider, useStoreState} from "easy-peasy";
 import {Provider} from "react-redux";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {HomePage} from "./pages/HomePage";
+import {Grommet} from "grommet";
 
 function AuthIsLoaded({children}) {
     const auth = useStoreState(state => state.firebase.auth)
@@ -12,19 +13,31 @@ function AuthIsLoaded({children}) {
     return children
 }
 
+const theme = {
+    global: {
+        font: {
+            family: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";',
+            size: '14px',
+            height: '20px',
+        },
+    },
+};
+
 const App = () => {
     return (
         <Provider store={store}>
             <StoreProvider store={store}>
                 <ReactReduxFirebaseProvider {...reactReduxFirebaseProps}>
                     <AuthIsLoaded>
-                        <BrowserRouter>
+                        <Grommet theme={theme}>
+                            <BrowserRouter>
                                 <Switch>
                                     <Route exact path='/'>
                                         <HomePage/>
                                     </Route>
                                 </Switch>
-                        </BrowserRouter>
+                            </BrowserRouter>
+                        </Grommet>
                     </AuthIsLoaded>
                 </ReactReduxFirebaseProvider>
             </StoreProvider>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,32 @@
+import "./HomePage.css";
+import {Box, Button, Card, CardBody, CardHeader, Header, Heading, Main, Paragraph} from "grommet";
+import React from "react";
+
 export const HomePage = () => {
-    return (<div>
-        Home
-    </div>)
+    return (
+        <React.Fragment>
+            <Header background="brand" pad="small">
+                <Heading level={3}>ledger.bet</Heading>
+                <Button secondary label="Sign Up"/>
+            </Header>
+            <Main pad="large">
+                <Card background="brand" pad="medium">
+                    <CardHeader textAlign="center">
+                        <Heading
+                            weight="bold"
+                            size="large"
+                            textAlign="center">
+                            ledger.bet
+                        </Heading>
+                    </CardHeader>
+                    <CardBody>
+                        <Heading size="small">
+                            make friendly bets
+                        </Heading>
+                        <Button secondary label="Sign Up"/>
+                    </CardBody>
+                </Card>
+            </Main>
+        </React.Fragment>
+    )
 }
