babaa828cf63ce85f5a587aaddf1cfa9319e3095
Donnie Mattingly <donniewmattingly@gmail.com>
2021-02-03 22:00:24 -0500

    better save logic

--- src/components/pools/SquaresPool.js
+++ src/components/pools/SquaresPool.js
@@ -28,14 +28,15 @@ const SquareCellContainer = styled.div`
   justify-content: center;
   align-items: center;
   align-content: center;
-  
+
   max-width: calc(800px / 13);
+  width: 4vw;
 
   :hover {
     cursor: pointer;
     background-color: #FFFFFF13;
   }
-  
+
   ${props => props.selected ? 'border: 1px solid white' : ''};
   border-radius: 0.3em;
   margin: 0.2em;
@@ -53,53 +54,103 @@ const SquareCell = (props) => {
     const profile = useStoreState(state => state.firebase.profile);
     const selected = selections.includes(num);
 
+    const onClick = () => {
+
+        if (!groupSelections[num]) {
+            onSelected(num);
+        }
+    }
+
     return (
-        <SquareCellContainer selected={selected} onClick={() => onSelected(props.num)}>
+        <SquareCellContainer selected={selected} onClick={onClick}>
             {selected
-                ? <Avatar round size={'30px'} name={profile.displayName}/>
+                ? <Avatar round size={'4vw'} name={profile.displayName}/>
                 : groupSelections[num]
-                    ? <Avatar round size={'30px'} name={groupSelections[num]}/>
+                    ? <Avatar round size={'4vw'} name={groupSelections[num]}/>
                     : '$5'}
         </SquareCellContainer>
     )
 }
 
-const Squares = (props) =>
-{
+const GridContainerRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`
+
+const GridContainerColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const TeamName = styled.div`
+  font-size: 1.4em;
+  font-weight: bold;
+`
+
+const HorizontalTeamName = styled(TeamName)`
+  writing-mode: vertical-rl;
+  text-orientation: upright;
+`
+
+const Squares = (props) => {
     const {selections, groupSelections, onSquareSelected} = props;
 
     return (
-        <SquareGrid>
-            <NumberCell x={1} y={1}/>
-            {[...Array(10).keys()].map(it => <NumberCell x={1} y={it + 2}> ? </NumberCell>)}
-            {[...Array(10).keys()].map(it => <NumberCell x={it + 2} y={1}> ? </NumberCell>)}
-            {[...Array(100).keys()].map(it => <SquareCell onSelected={onSquareSelected} num={it}
-                                                          groupSelections={groupSelections}
-                                                          selections={selections}/>)}
-        </SquareGrid>
+        <GridContainerRow>
+            <HorizontalTeamName> Cheifs</HorizontalTeamName>
+            <GridContainerColumn>
+                <TeamName> Bucs </TeamName>
+                <SquareGrid>
+                    <NumberCell x={1} y={1}/>
+                    {[...Array(10).keys()].map(it => <NumberCell x={1} y={it + 2}> ? </NumberCell>)}
+                    {[...Array(10).keys()].map(it => <NumberCell x={it + 2} y={1}> ? </NumberCell>)}
+                    {[...Array(100).keys()].map(it => <SquareCell onSelected={onSquareSelected} num={it}
+                                                                  groupSelections={groupSelections}
+                                                                  selections={selections}/>)}
+                </SquareGrid>
+            </GridContainerColumn>
+        </GridContainerRow>
     )
 }
 
 const SquaresHeader = styled.div`
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`
+
+const SelectedInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: end;
+  align-items: end;
+`
+
+const SelectedActions = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: end;
+  align-items: end;
 `
 
 const SquaresInfo = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  align-items: end;
 `
 
 const SaveSquaresButton = styled(SignUpButton)`
-margin-left: 1em;
+  margin-left: 1em;
 `
 
-export const SquaresPool = (props) =>
-{
+export const SquaresPool = (props) => {
     const {pool} = props;
     const auth = useStoreState(state => state.firebase.auth);
     const submitPoolEntry = useStoreActions(actions => actions.pools.submitPoolEntry);
@@ -113,11 +164,17 @@ export const SquaresPool = (props) =>
         setSelections(pool.members[auth.uid]?.selections ?? [])
     }, [pool])
 
-    const onSquareSelected = (square) => {
+    const onSquareSelected = async (square) => {
         if (selections.includes(square)) {
             setSelections(selections.filter(it => it !== square));
         } else if (selections.length < pool.maxSelections) {
-            setSelections([...selections, square]);
+            const newSelections = [...selections, square];
+            try {
+                await submitPoolEntry({poolId: pool.id, groupId: pool.groupId, selections: newSelections});
+            } catch (e) {
+                console.error(e);
+            }
+            setSelections(newSelections);
         }
     }
 
@@ -136,18 +193,26 @@ export const SquaresPool = (props) =>
                     Super Bowl Squares
                 </h2>
 
-                <div>
+                <SelectedInfo>
                     Selected: {selections.length} / {pool.maxSelections} (${selections.length * 5})
-                    {selections.length > 0 && <SaveSquaresButton onClick={onSave}> Save  </SaveSquaresButton>}
-                    {selections.length > 0 && <SaveSquaresButton onClick={() => setSelections([]) && onSave()}> Clear </SaveSquaresButton>}
-                </div>
+                    <SelectedActions>
+                        {selections.length > 0 &&
+                        <SaveSquaresButton onClick={() => setSelections([]) && onSave()}> Clear </SaveSquaresButton>}
+                    </SelectedActions>
+                </SelectedInfo>
             </SquaresHeader>
             <Squares {...{onSquareSelected, selections, groupSelections}}/>
             <SquaresInfo>
                 <p>
-                    Pick up to 10 squares, when the game starts each column and row will randomly be assigned one of
-                    0-9.
-                    If your Row / Column Combo is the score at the end of any quarter, you win that quarters payout.
+                    You can pick up to 10 squares. When the game starts each column and row will randomly be assigned
+                    a number 0-9. The final number of each teams score at the end of that quarter determines the winner.
+                    payouts for each quarter are:
+                </p>
+                <p>
+                    <span> Q1: 15%</span>
+                    <span> Q2: 30%</span>
+                    <span> Q3: 15%</span>
+                    <span> Q1: 40%</span>
                 </p>
             </SquaresInfo>
         </AppCell>
