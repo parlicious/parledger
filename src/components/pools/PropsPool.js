43ee7f051ed5976dd03106a57b3de9b80efeac54
Donnie Mattingly <donniewmattingly@gmail.com>
2021-02-08 08:35:27 -0500

    calculate pool points

--- src/components/pools/PropsPool.js
+++ src/components/pools/PropsPool.js
@@ -7,6 +7,58 @@ import {ConfirmButton, PrimaryButton} from '../../styles';
 import {addImpliedOddsToEvents} from '../../stores/wagers';
 import {UserAvatar} from '../UserAvatar';
 
+const results = {
+    "8036708": "737276821",
+    "8036729": "737277010",
+    "8036734": "737277560",
+    "8036752": "737277745",
+    "8036760": "737278418",
+    "8036767": "737279421",
+    "8036769": "737279647",
+    "8036771": "737279877",
+    "8036774": "737280160",
+    "8036782": "737280215",
+    "8036784": "737280336",
+    "8207144": "760485325",
+    "8207145": "760485938",
+    "8207149": "760486154",
+    "8207151": "760486297",
+    "8207156": "760487113",
+    "8227694": "763115351",
+    "8228006": "763128783",
+    "8228008": "763128812",
+    "8228667": "763241510",
+    "8228687": "763244235",
+    "8228695": "763247179",
+    "8228719": "763254542",
+    "8228734": "763256105",
+    "8228747": "763257503",
+    "8228768": "763262538",
+    "8228812": "763265961",
+    "8228826": "763268255",
+    "8228832": "763269386",
+    "8228836": "763269755",
+    "8228839": "763270139",
+    "8228848": "763271572",
+    "8234342": "764005213",
+    "8235687": "764184750",
+    "8235692": "764185321",
+    "8235694": "764185532",
+    "8240554": "764813833",
+    "8240556": "764814090",
+    "8244605": "765319573",
+    "8244607": "765320201",
+    "8244627": "765322746",
+    "8244629": "765323751",
+    "8244642": "765325838",
+    "8250705": "766121846",
+    "8250709": "766122041",
+    "8255073": "766717812",
+    "8286029": "771277311",
+    "8286030": "771277606",
+    "8286033": "771277787"
+}
+
 const PropsSelectionProgressContainer = styled.div`
   position: sticky;
 
@@ -101,8 +153,8 @@ const PoolMembers = (props) => {
                         <ScoreboardCell>
                             {member.info.displayName}
                         </ScoreboardCell>
-                        <ScoreboardCell> {calculatePossiblePoints(member.selections, section.events)}</ScoreboardCell>
-                        <ScoreboardCell> 0 </ScoreboardCell>
+                        <ScoreboardCell> {calculatePossiblePoints(member.selections, section.events)} </ScoreboardCell>
+                        <ScoreboardCell> {calculatePossiblePoints(member.selections, section.events)} </ScoreboardCell>
                     </React.Fragment>
                 )}
             </PoolMembersGrid>
@@ -125,7 +177,20 @@ const calculatePossiblePoints = (propsSelected, events) => {
 
 // TODO
 const calculateActualPoints = (propsSelected, events) => {
-    return 0;
+    console.log(propsSelected, events);
+    const raw = Object.keys(propsSelected).map(k => {
+            const event = events.find(it => it.id === k);
+            const outcome = event.displayGroups[0].markets[0].outcomes.find(it => it.id === propsSelected[k]);
+
+            if(results[k] === propsSelected[k]){
+                return 1 - outcome.impliedOdds
+            } else {
+                return 0;
+            }
+        }
+    ).reduce((a, b) => a + b, 0)
+
+    return Math.floor(raw * 100);
 }
 
 export const PropsPool = (props) => {
